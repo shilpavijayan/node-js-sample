0554f3655632420145c8f454513c80e1cf36259e
shilpam <shilpavijayan.14@gmail.com>
2013-10-10 20:31:40 +0000

    Changed to read from index

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var buf = fs.readFileSync('index.html');
+var str = buf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(str);
 });
 
-var port = process.env.PORT || 5000;
+port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
